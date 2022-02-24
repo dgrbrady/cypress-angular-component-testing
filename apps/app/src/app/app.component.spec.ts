a20e64e2437062daa7346327109267a82a5fdbfe
David Brady <dgrbrady@gmail.com>
2022-02-24 09:01:29 -0500

    working component tests in app

--- apps/app/src/app/app.component.spec.ts
+++ apps/app/src/app/app.component.spec.ts
@@ -1,30 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { initEnv, mount } from 'cypress-angular-unit-test';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    initEnv(AppComponent, {declarations: [NxWelcomeComponent]});
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = mount(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).to.exist;
   });
 
   it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = mount(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).to.equal('app');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome app');
+    const fixture = mount(AppComponent);
+    cy.get('h1').should('contain.text', 'Welcome app');
   });
 });
---
+++ cypress/plugins/index.ts
@@ -0,0 +1,13 @@
+import * as webpackConfig from './webpack.config';
+
+module.exports = (on, config) => {
+  const { startDevServer } = require('@cypress/webpack-dev-server');
+
+  on('dev-server:start', (options) =>
+    startDevServer({
+      options,
+      webpackConfig,
+    }),
+  );
+  return config;
+};
